97b60bc958367dc940e261084b207b234906cde7
Resultedjam <88166236+Resultedjam@users.noreply.github.com>
2021-12-20 00:33:19 -0600

    Update conexion.php

--- scripts/conexion.php
+++ scripts/conexion.php
@@ -1,5 +1,5 @@
 <?php
-	$mysqli=new mysqli("localhost","root", "","proyecto");
+	$mysqli=new mysqli("resultedjam.github.io","root", "","proyecto");
 	
 	if(mysqli_connect_errno()){
 		echo 'Fallo en la conexion : ', mysqli_connect_error();
